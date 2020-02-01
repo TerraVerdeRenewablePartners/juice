f2dba40529e82365de7e20eeebbbf08e53a3767f
Milo Toor <milo.toor@gmail.com>
2020-02-01 10:30:40 -0800

    CRA basics

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import { hasItem, setItems } from './localStorage';
+import ChargingPage from './ChargingPage';
+import Onboarding from './Onboarding';
+import './App.css';
+
+
+function App() {
+  // Check if the user has set their initial info
+  const hasOnboarded = hasItem('hasOnboarded');
+  const [showOnboarding, setShowOnboarding] = useState(!hasOnboarded);
+  
+  return (
+    <div className="App">
+      {showOnboarding
+        ? <Onboarding submit={saveOnboardData} />
+        : <ChargingPage />
+      }
+    </div>
+  );
+  
+  function saveOnboardData (data) {
+    setItems({
+      ...data,
+      hasOnboarded: true
+    });
+    
+    setShowOnboarding(false);
+  }
+}
+
+export default App;
