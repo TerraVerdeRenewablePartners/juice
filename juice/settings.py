6e042d6c6c4217e29b8e1602f5752cdba4bc09c7
Dylan Grayson <dylan@terraverde.energy>
2020-02-01 11:43:04 -0800

    CSV data and corsheaders

--- juice/settings.py
+++ juice/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
